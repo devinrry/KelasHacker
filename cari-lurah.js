8b24897df3fb36b652822dde794dcfceb858044f
devinrry <devi.alannurry27@gmail.com>
2021-06-17 00:39:49 +0700

    commit satu

---
+++ cari-lurah.js
@@ -0,0 +1,8 @@
+function cariLurahBaru(calons) {
+    // your code here
+}
+
+console.log(cariLurahBaru([1, 2, 3, 2, 1, 3, 2, 3])); // pemilihan ulang
+console.log(cariLurahBaru([1, 2, 3, 2, 1, 3, 3])); // 3
+console.log(cariLurahBaru([1])); // 1
+console.log(cariLurahBaru([])); // -1
